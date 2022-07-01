8ddae6bc736579d370a2bf9dafc217803affc458
Doris Okodugha <digitaloryx@gmail.com>
2022-07-01 06:51:31 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h> 
+/**
+ * main - prints a standard error
+ * Return: Always 1 (Successful)
+ * i know that i can make it
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+	return (1);
+}
+fprintf(stderr, "and that piece of art is useful\" - \
+		Dora Korpar, 2015-10-19\n");
+return (1);
+}
