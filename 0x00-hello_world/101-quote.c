34d22f3a50b75c8e1b5a5cf9e6e652ca8842c5fd
Doris Okodugha <digitaloryx@gmail.com>
2022-07-01 07:19:10 -0700

    0-positive_or_negative.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  * main - Entry point
  * Return: Always 1 (Successful)
  * i made it ooooo
- */
+*/
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[]="and that piece of art is useful\"-Dora Korpar,2015-10-19\n";
 	write(2, str1, 59);
 	return (1);
 }
