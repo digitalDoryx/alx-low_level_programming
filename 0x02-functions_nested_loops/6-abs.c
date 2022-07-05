11ecc61e63ec30cd602108136ecef963455f4d39
Doris Okodugha <digitaloryx@gmail.com>
2022-07-05 02:45:49 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "putchar.h"
+
+/**
+ * 6_abs - returns absolute value of an int
+ *
+ * @n: integer from which to get an absolute value
+ *
+ * Return: Void.
+ */
+
+int 6_abs(int n)
+
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	
+	return (n);
+
+}
