1136d2696eb476cdffdb3530c66401038b31cb5a
Doris Okodugha <digitaloryx@gmail.com>
2022-07-05 01:54:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "putchar.h"
+
+/**
+ * main - Prints Putchar
+ *
+ * Return: Always 0 (Success)
+ * I am printing Putchar
+ */
+
+int main(void)
+
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
