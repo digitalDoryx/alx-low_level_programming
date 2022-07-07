edaaf58ec1dd6617af9df0545ed2ef98ee04bb40
digitalDoryx <106775455+digitalDoryx@users.noreply.github.com>
2022-07-07 21:42:09 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,6 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-<<<<<<< HEAD
  * _isupper - define if a character is uppercase
  *
  * Description: Prints the alphabet with _putchar
@@ -15,29 +14,88 @@ int _isupper(int c)
 		return (1);
 	else
 		return (0);
-=======
-
- * _isupper - define if a character is uppercase
-
- *
-
- * Description: Prints the alphabet with _putchar
-
- * @c: charater
-
- * Return: void
-
- */
-
-int _isupper(int c)
-
-{
-
-	if (c >= 'A' && c <= 'Z')		return (1);
-
-	else
-
-		return (0);
-
->>>>>>> 32bc9766900a4b452137dae572d1bed8cbf908aa
 }
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+		
+
+
+
+		
+
