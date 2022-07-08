b942eaaea12db576d793e500003bfd1cb798a937
Doris Okodugha <digitaloryx@gmail.com>
2022-07-08 09:31:40 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,119 +1,21 @@
-<<<<<<< HEAD
-/**
- * _isupper - check if c is upper
- *
- * @c: input for alphabet
- *
- * Return: 1 if its uppercase, 0 if not
- */
-
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-		return (1);
-	else
-		return (0);
-}
-=======
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isupper - define if a character is uppercase
- *
- * Description: Prints the alphabet with _putchar
- * @c: charater
- * Return: void
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+char x = 'A';
+int upper = 0;
+for (; x <= 'Z'; x++)
+{
+if (c == x)
+{
+upper = 1;
+break;
 }
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-		
-
-
-
-		
-
->>>>>>> edaaf58ec1dd6617af9df0545ed2ef98ee04bb40
+}
+return (upper);
+}
