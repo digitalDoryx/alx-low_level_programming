8fe637260ded2904ce92be30b93351b5e57859ee
Doris Okodugha <digitaldoryx@gmail.com>
2022-07-12 14:14:44 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,38 @@
-#import "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define GRAPH_MIN 33
+#define GRAPH_MAX 126
+
 /**
- * main -
- * Return:
+ * main - generate valid passwords for 101-crackme
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	return (0);
+int sum = 2772;
+char c;
+
+srand(time(NULL));
+
+while (sum > GRAPH_MAX)
+{
+c = rand() % (GRAPH_MAX - GRAPH_MIN) + GRAPH_MIN;
+
+sum -= c;
+
+if (sum < GRAPH_MIN)
+{
+c -= (GRAPH_MIN - sum);
+sum = GRAPH_MIN;
+}
+putchar(c);
+
+}
+putchar(sum);
+
+return (0);
+
 }
