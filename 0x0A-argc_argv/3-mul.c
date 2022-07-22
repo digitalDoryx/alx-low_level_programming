6a382647a3775cf51b9fb893d6d38ba7269e56b5
Doris Okodugha <106775455+digitalDoryx@users.noreply.github.com>
2022-07-22 13:03:06 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - multiplies two numbers
+ * main - adds positive numbers
  * @argc: count
  * @argv: array
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add = add + atoi(argv[i]);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", add);
 	return (0);
 }
