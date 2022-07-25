5d7127f634a1bf5de29b56880637a5386be8392a
Doris Okodugha <106775455+digitalDoryx@users.noreply.github.com>
2022-07-25 12:49:05 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,24 +1,22 @@
-
 /*
- *  * File: 4-free_grid.c
- *   * Auth: Gedeon Obae Gekonge
- *    */
+ * File: 4-free_grid.c
+ * Auth: Gedeon Obae Gekonge
+ */
 
 #include "main.h"
 #include <stdlib.h>
 
 /**
- *  * free_grid - Frees a 2-dimensional array of integers.
- *   * @grid: The 2-dimensional array of integers to be freed.
- *    * @height: The height of grid.
- *     */
+ * free_grid - Frees a 2-dimensional array of integers.
+ * @grid: The 2-dimensional array of integers to be freed.
+ * @height: The height of grid.
+ */
 void free_grid(int **grid, int height)
 {
-		int index;
+	int index;
 
-			for (index = 0; index < height; index++)
-						free(grid[index]);
+	for (index = 0; index < height; index++)
+		free(grid[index]);
 
-				free(grid);
+	free(grid);
 }
-
