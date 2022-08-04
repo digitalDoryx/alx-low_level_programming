678ddf3fe9e90c47415f40f52a42a309e6b2f5b3
Doris Okodugha <106775455+digitalDoryx@users.noreply.github.com>
2022-08-04 15:08:16 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,45 +1,25 @@
-#ifndef VARIADIC_FUNCTIONS_H
-
-#define VARIADIC_FUNCTIONS_H
-
+#include "variadic_functions.h"
 #include <stdarg.h>
 
-
-
 /**
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
- *  * struct printer - A new struct type defining a printer.
- *
- *   * @symbol: A symbol representing a data type.
- *
- *    * @print: A function pointer to a function that prints
- *
- *     *         a data type corresponding to symbol.
- *
- *      */
-
-typedef struct printer
-
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
 {
+	va_list nums;
+	unsigned int index, sum = 0;
 
-		char *symbol;
+	va_start(nums, n);
 
-			void (*print)(va_list arg);
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
 
+	va_end(nums);
 
-
-} printer_t;
-
-
-
-int sum_them_all(const unsigned int n, ...);
-
-void print_numbers(const char *separator, const unsigned int n, ...);
-
-void print_strings(const char *separator, const unsigned int n, ...);
-
-void print_all(const char * const format, ...);
-
-
-
-#endif
+	return (sum);
+}
