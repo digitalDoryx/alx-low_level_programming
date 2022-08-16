22f9d699f645d367be5956e3e6d049508f1f3784
Doris Okodugha <digitaldoryx@gmail.com>
2022-08-16 03:35:28 -0700

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,17 @@
+es (80 sloc)  1.65 KB
+
+
+
+#include "lists.h"
+
+
+
+size_t looped_listint_count(listint_t *head);
+
+size_t free_listint_safe(listint_t **h);
+
+
+
+/**
+ *
+ *  * looped_listint_co
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+
+/**
+ *
+ *  * find_listint_loop - Finds the loop contained in
+ *
+ *   *                     a listint_t linked list.
+ *
+ *    * @head: A pointer to the head of the listint_t list.
+ *
+ *     *
