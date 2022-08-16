1717146f5000bcdbfce784662a3b3c34c0e98545
Doris Okodugha <digitaldoryx@gmail.com>
2022-08-16 04:19:59 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+
+/**
+ *
+ *  * sum_listint - Calculates the sum of all the
+ *
+ *   *               data (n) of a listint_t list.
+ *
+ *    * @head: A pointer to the head of the listint_t list.
+ *
+ *     *
