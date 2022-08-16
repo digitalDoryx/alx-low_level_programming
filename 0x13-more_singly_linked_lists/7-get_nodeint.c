076fb530e8928d6d230940c23e9d1e9fd2849d86
Doris Okodugha <digitaldoryx@gmail.com>
2022-08-16 04:18:30 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+
+
+/**
+ *
+ *  * get_nodeint_at_index - Locates a given node of
+ *
+ *   *                        a listint_t linked list.
+ *
+ *    * @head: A pointer to the head of the listint_t list.
+ *
+ *     * @index: The index of the node to locate - indices start at 0.
+ *
+ *      *
