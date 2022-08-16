34807ed47550f0c5431cc326f00aa42b79ad2c1d
Doris Okodugha <digitaldoryx@gmail.com>
2022-08-16 04:21:08 -0700

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+
+/**
+ *
+ *  * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *
+ *   *                           list at a given position.
+ *
+ *    * @head: A pointer to the address of the
+ *
+ *     *        h
