2394538803e7b088444fbba07a8546de77a84819
Doris Okodugha <106775455+digitalDoryx@users.noreply.github.com>
2022-08-19 13:57:45 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,47 +1,24 @@
 #include "main.h"
 
-
-
 /**
+ * get_bit - returns the value of a bit at a given index
+ * @n: unsigned long int
+ * @index: the index
  *
- *  * get_bit - returns the value of a bit at a given index
- *
- *   * @n: unsigned long int
- *
- *    * @index: the index
- *
- *     *
- *
- *      * Return: the value of the bit at index index or -1 if an error occured
- *
- *       */
-
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
 int get_bit(unsigned long int n, unsigned int index)
-
 {
+	unsigned int i;
 
-		unsigned int i;
-
-
-
-			if (n == 0 && index < 64)
-
-						return (0);
-
-				for (i = 0; i <= 63; n >>= 1, i++)
-
-						{
-
-									if (index == i)
-
-												{
-
-																return (n & 1);
-
-																		}
-
-										}
-
-					return (-1);
-
+	if (n == 0 && index < 64)
+		return (0);
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
 }
